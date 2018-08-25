879f88b995e972be93ceb49aa05952c22e0abeec
MrHidalgo <Luckyhidalgo@gmail.com>
2018-08-25 19:30:14 +0300

    Sidebar - add logic for open.

--- dest/js/app.js
+++ dest/js/app.js
@@ -5,10 +5,53 @@
  * @description Init hamburger logic with animated
  */
 function initHamburger() {
-  var btn = document.querySelector("[hamburger-js]");
+  var btn = document.querySelector("[hamburger-js]"),
+      sideBar = document.getElementById("sidebar");
 
   btn.addEventListener("click", function (e) {
     e.currentTarget.classList.toggle("is-active");
+
+    sideBar.classList.toggle("is-open");
+  });
+}
+
+/**
+ * @name initBodyClickClosest
+ * @description
+ */
+function initBodyClickClosest() {
+  var body = document.getElementsByTagName("body")[0];
+
+  var strSelectorClosest = ".sidebar, .hamburger";
+
+  body.addEventListener("click", function (ev) {
+    var evTarget = ev.target.closest(strSelectorClosest),
+        sidebar = document.querySelectorAll(".sidebar");
+
+    if (!evTarget) {
+      sidebar.forEach(function (item) {
+        item.classList.remove("is-open");
+      });
+    }
+  });
+}
+
+/**
+ * @name initPreventBehavior
+ * @description
+ */
+function initPreventBehavior() {
+
+  var link = document.querySelectorAll("a");
+
+  link.forEach(function (val, idx) {
+
+    val.addEventListener("click", function (e) {
+
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
   });
 }
 
@@ -39,6 +82,8 @@ function initHamburger() {
    * @description Init all method
    */
   var initNative = function initNative() {
+    initPreventBehavior();
+    initBodyClickClosest();
     initHamburger();
     // ...
   };
