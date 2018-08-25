f66b83ab57f9f4d76dbd3cdc4103a31e6128abe1
MrHidalgo <Luckyhidalgo@gmail.com>
2018-08-25 18:59:53 +0300

    Header - create: add hamburger & logotype. Sidebar - create: add svg & hover.

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,6 +1,52 @@
+"use strict";
+
+/**
+ * @name initHamburger
+ * @description Init hamburger logic with animated
+ */
+function initHamburger() {
+  var btn = document.querySelector("[hamburger-js]");
+
+  btn.addEventListener("click", function (e) {
+    e.currentTarget.classList.toggle("is-active");
+  });
+}
+
+/**
+ * @description Document DOM ready.
+ */
+(function () {
+  /**
+   *
+   * @type {*|jQuery|HTMLElement}
+   * @private
+   */
+  var _document = document,
+      _window = window;
+
+  /**
+   * =============================================
+   * CALLBACK
+   * =============================================
+   */
+
+  /**
+   * =============================================
+   * =============================================
+   */
+
+  /**
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    initHamburger();
+    // ...
+  };
+  initNative();
+})();
+
 /*!
 *
 * Include lib:
 *
 * */
-"use strict";
