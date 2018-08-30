a0b1450f51ba7576393371f9ca316674032697b6
MrHidalgo <Luckyhidalgo@gmail.com>
2018-08-30 13:02:54 +0300

    Create Messages tmpl.

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 require('./arrow')
+require('./attach')
 require('./call')
 require('./clock-small')
 require('./dashboard')
@@ -7,5 +8,6 @@ require('./doc')
 require('./img-empty')
 require('./message')
 require('./program')
+require('./send')
 require('./toolkit')
 require('./wall')
--- src/views/coach/messages/messages.vue
+++ src/views/coach/messages/messages.vue
@@ -3,6 +3,262 @@
   <div class="c-grid">
     <div class="messages__wrapper">
       <h1 class="messages__title title">Messages</h1>
+      <p class="messages__subtitle">Messages with anyone on your team at any time</p>
+
+      <div class="messages__chat">
+
+        <div class="messages__chat-left">
+
+          <div>
+            <p class="messages__chat-title">Conversations</p>
+            <a href="#" class="messages__chat-mark">mark all read</a>
+          </div>
+
+          <div>
+            <ul class="messages__chat-user">
+              <li class="is-unread">
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Marco Chavez</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias architecto, cumque exercitationem nesciunt odio ratione repellat? A adipisci porro sit!</p>
+                  </div>
+                  <div>
+                    <p>6:32 PM</p>
+                  </div>
+                </a>
+              </li>
+              <li class="is-active">
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Maria Perales</p>
+                    <p>Start a conversation</p>
+                  </div>
+                  <div>
+                    <p>March 14</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Michael</p>
+                    <p>You: Sent a media</p>
+                  </div>
+                  <div>
+                    <p>March 14</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Michael</p>
+                    <p>You: Sent a media</p>
+                  </div>
+                  <div>
+                    <p>Feb 28</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Michael</p>
+                    <p>You: Sent a media</p>
+                  </div>
+                  <div>
+                    <p>Feb 28</p>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <div>
+                    <div class="messages__chat-img">
+                      <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                    </div>
+                  </div>
+                  <div>
+                    <p>Michael</p>
+                    <p>You: Sent a media</p>
+                  </div>
+                  <div>
+                    <p>Feb 28</p>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+        </div>
+        <div class="messages__chat-right">
+
+          <div class="messages__chat-box">
+
+            <div class="messages__chat-box--head">
+
+              <div>
+                <div class="messages__chat-img">
+                  <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                </div>
+
+                <p>Michael</p>
+              </div>
+
+              <div>
+                <a href="#">See profile</a>
+              </div>
+
+            </div>
+
+
+            <div class="messages__chat-box--body">
+
+              <div class="messages__chat-text">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Hi Christian!</p>
+                    <p>Do you have time tomorrow for talk?</p>
+                    <span>20 min</span>
+                  </div>
+                </div>
+
+              </div>
+              <div class="messages__chat-text messages__chat-text--reverse">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Yes of course, tomorrow. I can 2:00 PM, this work?</p>
+                    <span>5 min</span>
+                  </div>
+                </div>
+
+              </div>
+              <div class="messages__chat-text">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Perfect!</p>
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="messages__chat-text">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Hi Christian!</p>
+                    <p>Do you have time tomorrow for talk?</p>
+                    <span>20 min</span>
+                  </div>
+                </div>
+
+              </div>
+              <div class="messages__chat-text messages__chat-text--reverse">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Yes of course, tomorrow. I can 2:00 PM, this work?</p>
+                    <span>5 min</span>
+                  </div>
+                </div>
+
+              </div>
+              <div class="messages__chat-text">
+
+                <div>
+                  <div class="messages__chat-img">
+                    <img src="@/assets/img/img-profileHeader.jpg" srcset="@/assets/img/img-profileHeader@2x.jpg 2x" alt="">
+                  </div>
+                </div>
+                <div>
+                  <div class="messages__chat-text--notice">
+                    <p>Perfect!</p>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+
+
+            <div class="messages__chat-box--footer">
+
+              <div>
+                <form action="">
+                  <input type="text" placeholder="Type a message"></input>
+                </form>
+              </div>
+
+              <div>
+                <a href="#">
+                  <svgicon name="attach" class="icon messages__chat-box--attach" :original="true"/>
+                </a>
+                <a href="#">
+                  <svgicon name="send" class="icon messages__chat-box--send" :original="true"/>
+                </a>
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   </div>
 </section>
